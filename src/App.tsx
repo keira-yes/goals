b40d011708f6e9755ac3dbc91a329cac9874d2a2
Irina Kirillova <keira.kirillova@gmail.com>
2024-01-12 14:58:04 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from './components/Header'
 import { Goal } from './components/Goal'
 
 function App() {
     return (
         <>
+            <Header />
             <Goal title='Main Goal'>Learn React + Typescript</Goal>
         </>
     )
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { type FC } from 'react'
+import user from '../assets/user.svg'
+
+export const Header: FC = () => {
+    return (
+        <header>
+            <div>
+                <h1>Hello Josh!</h1>
+                <p>It’s good to see you again.</p>
+            </div>
+            <figure>
+                <img src={user} alt='User' />
+            </figure>
+        </header>
+    )
+}
