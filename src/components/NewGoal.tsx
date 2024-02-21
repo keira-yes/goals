b9ebc33198628e13b26953507e59fb2d3d087c3b
Irina Kirillova <keira.kirillova@gmail.com>
2024-02-21 18:01:09 +0100

    add styled components for new goal form

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,8 +1,53 @@
 import { useRef, type FC, type FormEvent } from 'react'
+import styled from 'styled-components'
 
 type NewGoalProps = {
     addGoal: (title: string, description: string) => void
 }
+
+const Form = styled.form`
+    padding: 30px;
+    border-radius: 14px;
+    background-color: var(--light-grey);
+
+    Button {
+        width: 100%;
+        margin-top: 10px;
+    }
+`
+
+const Input = styled.input`
+    display: block;
+    width: 100%;
+    padding: 10px 15px;
+    border: 2px solid var(--black);
+    border-radius: 8px;
+    background-color: transparent;
+`
+
+const Label = styled.label`
+    display: block;
+    font-family: var(--font-secondary);
+`
+
+const Button = styled.button`
+    padding: 12px 35px;
+    border: 2px solid var(--black);
+    border-radius: 8px;
+    font-weight: 700;
+    font-size: 13px;
+    transition: background-color 0.3s ease, color 0.3s ease;
+`
+
+const PrimaryButton = styled(Button)`
+    background-color: var(--black);
+    color: var(--white);
+
+    &:hover {
+        background-color: transparent;
+        color: var(--dark);
+    }
+`
 
 export const NewGoal: FC<NewGoalProps> = ({ addGoal }) => {
     const goalTitleRef = useRef<HTMLInputElement>(null)
@@ -18,14 +63,14 @@ export const NewGoal: FC<NewGoalProps> = ({ addGoal }) => {
     }
 
     return (
-        <form name='new-goal' onSubmit={submitHandler}>
+        <Form name='new-goal' onSubmit={submitHandler}>
             <p>
-                <label htmlFor='goal-title'>Goal Title</label>
-                <input type='text' id='goal-title' name='goal-title' ref={goalTitleRef} required />
+                <Label htmlFor='goal-title'>Goal Title</Label>
+                <Input type='text' id='goal-title' name='goal-title' ref={goalTitleRef} required />
             </p>
             <p>
-                <label htmlFor='goal-description'>Goal Description</label>
-                <input
+                <Label htmlFor='goal-description'>Goal Description</Label>
+                <Input
                     type='text'
                     id='goal-description'
                     name='goal-description'
@@ -33,8 +78,8 @@ export const NewGoal: FC<NewGoalProps> = ({ addGoal }) => {
                 />
             </p>
             <p>
-                <button type='submit'>Add Goal</button>
+                <PrimaryButton type='submit'>Add Goal</PrimaryButton>
             </p>
-        </form>
+        </Form>
     )
 }
