5f12dbed4d8ba1cdbcf31f6ce9186bb3cfa77271
Irina Kirillova <keira.kirillova@gmail.com>
2024-07-06 12:12:53 +0200

    add styled component to goal

--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -1,4 +1,5 @@
 import { type FC, type PropsWithChildren } from 'react'
+import styled from 'styled-components'
 
 type GoalProps = PropsWithChildren<{
   id: number
@@ -6,14 +7,43 @@ type GoalProps = PropsWithChildren<{
   deleteGoalHandler: (id: number) => void
 }>
 
+const GoalStyled = styled.li`
+  padding: 20px;
+  border-radius: 14px;
+  background-color: var(--light-grey);
+
+  article {
+    position: relative;
+    padding-right: 70px;
+  }
+
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    font-family: var(--font-secondary);
+  }
+
+  h2 {
+    margin-bottom: 10px;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+`
+
 export const Goal: FC<GoalProps> = ({ id, title, deleteGoalHandler, children }) => {
   return (
-    <article>
-      <h2>{title}</h2>
-      {children}
-      <button type='button' onClick={() => deleteGoalHandler(id)}>
-        X
-      </button>
-    </article>
+    <GoalStyled>
+      <article>
+        <h2>{title}</h2>
+        {children}
+        <button type='button' onClick={() => deleteGoalHandler(id)}>
+          Remove
+        </button>
+      </article>
+    </GoalStyled>
   )
 }
--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -2,11 +2,21 @@ import { type FC } from 'react'
 import { Goal } from './Goal'
 import { type GoalType } from '../App'
 import { InfoBox } from './InfoBox'
+import styled from 'styled-components'
 
 type GoalsProps = {
   goals: GoalType[]
   deleteGoalHandler: (id: number) => void
 }
+
+const GoalsStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`
 
 export const Goals: FC<GoalsProps> = ({ goals, deleteGoalHandler }) => {
   if (goals.length === 0) {
@@ -18,18 +28,17 @@ export const Goals: FC<GoalsProps> = ({ goals, deleteGoalHandler }) => {
       {goals.length > 2 && <InfoBox type='warning' />}
       <section>
         <h2>Goals</h2>
-        <ul>
+        <GoalsStyled>
           {goals.map((goal) => (
-            <li key={goal.id}>
-              <Goal
-                id={goal.id}
-                title={goal.title}
-                deleteGoalHandler={deleteGoalHandler}>
-                {goal.description}
-              </Goal>
-            </li>
+            <Goal
+              key={goal.id}
+              id={goal.id}
+              title={goal.title}
+              deleteGoalHandler={deleteGoalHandler}>
+              {goal.description}
+            </Goal>
           ))}
-        </ul>
+        </GoalsStyled>
       </section>
     </>
   )
